ed0f3588041191e1ea7032ef587b06d48b5b79ce
Rodolphe Vidal <54648689+Setsin@users.noreply.github.com>
2020-01-10 14:52:30 +0100

    Add files via upload

---
+++ Exercice Julien/Galaxy/Galaxy_ResNet50.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 14:37:45 2020
+
+@author: Rodolphe
+"""
+
+# Import modules
+# Import Dataset
+# Create Short list
+# Create class
+# Returns an numpy array of train and test data
+# Creating a Training Set
+# Split Training / Val
+# Specify Model
+# Say not to train first layer (ResNet) model. It is already trained
+galaxy_model.layers[0].trainable = False
+# Compile Model
+# Model Sum
+# Fit Model
+# Plot Results
+# Summarize history for loss
+# Testing Model on the Test Data
+
+# Submission of the model to csv
